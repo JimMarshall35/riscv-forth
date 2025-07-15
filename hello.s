950bd474f9ddc644c0701d45cd04150160c70ab8
Jim Marshall <jimmarshall35@gmail.com>
2025-07-15 09:18:45 +0100

    vm restarted

--- hello.s
+++ hello.s
@@ -8,43 +8,17 @@ _start:
     call main
 
 main:
-     li sp, RAM_END
-     li a0, UART_BASE
-     call initUart
-     li a1, UART_BASE
-#     li a0, '\n'
-#     call putc
-#     li a0, 'J'
-#     call putc
-#     li a0, 'i'
-#     call putc
-#     li a0, 'm'
-#     call putc
-#     li a0, '\n'
-#     call putc
+    li sp, RAM_END
+    li a0, UART_BASE
+    call initUart
 
-     la a0, helloworld
-     call puts
- loop: 
-#     li a0, UART_BASE
-#     call getc_block
-#     li t1, '\n'
-#     beq a0, t1, 1f
-#     li t1, '\r'
-#     beq a0, t1, 1f
-#     li a1, UART_BASE
-#     call putc
-#     j 2f
-# 1: 
-#     li a0, '\r'
-#     li a1, UART_BASE
-#     call putc
-#     li a0, '\n'
-#     li a1, UART_BASE
-#     call putc
-# 2:
-# 3:
-    call outer_interpreter
+    call vm_init
+
+    li a1, UART_BASE
+    la a0, helloworld
+    call puts
+
+ loop:
     j loop
     
     ret
--- vm.s
+++ vm.s
@@ -1,235 +1,88 @@
-
-
 .include "defines.s"
-.global outer_interpreter
-.section .data
-.equ DataStackSize, 128
 
-.equ OIState_WaitingForLine,   0
-.equ OIState_InterpretingLine, 1
-.equ OuterInterpreterErrorBufferSize, 256
-.equ LineInputBufSize, 256
-.equ TokenBufSize, 32 
+#define VM_STACK_BOUNDS_CHECK
 
-dictionaryListHead: .word 0
-dictionaryListTail: .word 0
-vmflags: .word 0
-dictTop: .word _dataEnd + DataStackSize
-dataStackPtr: .word _dataEnd
-outerInterpreterErrorBuf: .skip OuterInterpreterErrorBufferSize,0
-lineInputBuf: .skip LineInputBufSize, 0
-tokenBuffer:  .skip TokenBufSize, 0
-lineInputParsePointer: .word 0 # points to the next token to be loaded
-OIState: .word OIState_WaitingForLine # outer interpreter state. We wait for a line to be input with "\n" 
-string_msg: .string "lololol\n"
+.global vm_init
 
-.section .text
+.equ CELL_SIZE, 4
+.equ DATA_STACK_MAX_SIZE, 32
+.equ RETURN_STACK_MAX_SIZE, 64
 
-init_vm:
-    li t0, OIState_WaitingForLine
-    la t1, OIState
-    sw t0, 0(t1)
-    call init_dict
-    ret
+.equ HEADER_NAME_BUF_SIZE, 32
+.equ HEADER_CODE_BUF_SIZE, 32
 
-init_dict:
-    
-    ret
+.equ DATA_STACK_MAX_SIZE_BYTES, (DATA_STACK_MAX_SIZE * CELL_SIZE)
 
-outer_interpreter_error_handler:
-    la a0, outerInterpreterErrorBuf
-    li a1, UART_BASE
-    call puts
-1:   
-    j 1b
-    ret
+.equ PC_REG, s0
+.equ THREAD_REG, s1
 
-outer_interpreter_fatal_error:
-    # Args:
-    # a0 - error string
-    la a1, outerInterpreterErrorBuf
-    li a2, OuterInterpreterErrorBufferSize
-    call strcpy_max
-    call outer_interpreter_error_handler
-    ret
-    
-outer_interpreter_waitingForLine:
-    SaveReturnAddress
+
+.data
+
+error_msg_stack_overflow:  .ascii "data stack overflow"
+error_msg_stack_underflow: .ascii "data stack underflow"
+error_msg_return_stack_overflow:  .ascii "return stack overflow"
+error_msg_return_stack_underflow: .ascii "return stack underflow"
+
+
+vm_data_stack: .fill DATA_STACK_MAX_SIZE, CELL_SIZE, 0      # 32 cell stack size
+vm_return_stack: .fill RETURN_STACK_MAX_SIZE, CELL_SIZE, 0  # 64 cell return stack size
+
+# data stack
+vm_p_data_stack_base: .word 0
+vm_data_stack_size: .word 0
+
+# return stack
+vm_p_return_stack_base: .word 0
+vm_return_stack_size: .word 0
+
+# dictionary
+vm_p_dictionary_start: .word 0
+vm_p_dictionary_end: .word 0
+
+
+vm_str_error_message: .word 0
+
+.macro StackSizeElements regSize, regTemp, regOut
+li \regTemp, CELL_SIZE
+div \regOut, \regSize, \regTemp 
+.endm
+
+.macro PushStack regBase, regSize, regVal
+#ifdef VM_STACK_BOUNDS_CHECK
+    li t0, DATA_STACK_MAX_SIZE_BYTES
+    bge \regSize, t0, 1f
+    j 2
 1:
-    li a0, UART_BASE
-    call getc_block
-    li t0, '\r' 
-    beq a0, t0, newline_entered
-
-    la t0, lineInputParsePointer
-    lw t1, 0(t0)
-    mv t2, t1
-    addi t2, t2, 1
-    li t3, LineInputBufSize
-    beq t3, t2, 2f # new char can't fit
-    la t3, tokenBuffer # t3 = ptr to where new char goes
-    add t3, t3, t1 # old line InputParsePtr value 
-    sw a0, 0(t3) # store char
-    sw t2, 0(t0) # store incremented size
-
-    li a1, UART_BASE
-    call putc
-    j 2f
-newline_entered:
-    li a0, '\r'
-    li a1, UART_BASE
-    call putc
-    li a0, '\n'
-    li a1, UART_BASE
-    call putc
-    la t0, OIState
-    li t1, OIState_InterpretingLine
+    l1 t0, vm_str_error_message
+    la t1, error_msg_stack_underflow
     sw t1, 0(t0)
-    la t0, lineInputParsePointer
-    sw zero, 0(t0)
-    j 3f
+    call vm_error_handler
 2:
-    j 1b
-3:
-    RestoreReturnAddress
-    ret
+#endif
+    add \regBase, \regBase, \regSize
+    sw \regVal, 0(\regBase)
+    addi \regSize, \regSize, CELL_SIZE
 
+.endm
 
-
-byte_at_parse_pointer:
-    # Returns:
-    # a0 - byte at parse ptr
-    SaveReturnAddress
-    PushReg t0
-    PushReg t1
-    la t1, lineInputBuf
-    la t0, lineInputParsePointer
-    lw t0, 0(t0)
-    add t1, t1, t0
-    lb a0, 0(t1)
-    PopReg t1
-    PopReg t0
-    RestoreReturnAddress
-    ret
-
-incr_parsepointer:
-    SaveReturnAddress
-    PushReg t0
-    PushReg t1
-    la t0, lineInputParsePointer
-    lw t0, 0(t0)
-    addi t0, t0, 1
-    la t1, lineInputParsePointer
-    sw t0, 0(t1) 
-    PushReg t0
-    PushReg t1
-    RestoreReturnAddress
-    ret
-
-is_whitespace:
-    # Args:
-    # a0 - char
-    # a1 - 1 if whitespace 0 if not
-    SaveReturnAddress
-    PushReg t1
-    li t1, ' '
-    beq a0, t1, iswhitespace
-    li t1, '\n'
-    beq a0, t1, iswhitespace
-    li t1, '\t'
-    beq a0, t1, iswhitespace
-    li t1, '\r'
-    beq a0, t1, iswhitespace
-    j isnotWhitespace
-iswhitespace:
-    li a1, 1
-    j 1b
-isnotWhitespace:
-    li a1, 0
-1:
-    PopReg t1
-    RestoreReturnAddress
-    ret
-
-
-load_next_token:
-    # Returns:
-    # a0 - 1 if token loaded, 0 if not
-    SaveReturnAddress
-    li t3, 0
-    li t4, 0
-    la t5, tokenBuffer
-lt_loop_start:
-    
-    call byte_at_parse_pointer
-    call is_whitespace
-    beq a1, zero, 1f
-    # is whitespace
-    bne t3, zero, 2f
-    call incr_parsepointer
-    
-    j lt_loop_start
-1:
-    # not whitespace
-    li t3, 1
-    beqz a0, 2f
-    add t6, t5, t4
-    sb a0, 0(t6)
-    addi t4, t4, 1
-    j lt_loop_start
-2:
-    add t6, t5, t4
-    sb zero, 0(t6)
-
-    RestoreReturnAddress
-    ret
-
-outer_interpreter_interpretingLine:
-    SaveReturnAddress
-    la a0, string_msg
-    li a1, UART_BASE
-    call puts 
-1:
-    call load_next_token
-    beqz a0, 2f
-    # new token loaded in buffer
-    la a0, tokenBuffer
-    li a1, UART_BASE
-    call puts 
-    j 1b
-2:
-    la t0, OIState
-    li t1, OIState_WaitingForLine
-    sw t1, 0(t0)
-    la t0, lineInputParsePointer
-
-    sw zero, 0(t0)
-    RestoreReturnAddress
-    ret
-
-outer_interpreter:
-    SaveReturnAddress
-1:
-    la t0, OIState
-    lw t1, 0(t0)
-    li t2, OIState_WaitingForLine
-    beq t2, t1, OI_WaitingForLine
-    li t2, OIState_InterpretingLine
-    beq t2, t1, OI_DoingLine
-OI_WaitingForLine:
-    la a0, lineInputBuf
-    li a1, 0
-    li a2, LineInputBufSize
-    call memset
-    call outer_interpreter_waitingForLine
+.macro PopStack regBase, regSize, regOutVal
+    addi \regSize, \regSize, -CELL_SIZE
+#ifdef VM_STACK_BOUNDS_CHECK
+    blt \regSize, \regBase, 1f
     j 2f
-OI_DoingLine:
-    call outer_interpreter_interpretingLine
+1:
+    la t0, vm_str_error_message
+    la t1, error_msg_stack_underflow
+    sw t1, 0(t0)
+    call vm_error_handler
 2:
-    j 1b
+#endif
+    add \regBase, \regBase, \regSize
+    lw \regOutVal, 0(\regBase)
+.endm
 
-    RestoreReturnAddress
-    ret 
+
 
 .macro padded_string string, max
 1:
@@ -245,47 +98,158 @@ OI_DoingLine:
 
 .endm
 
+# Imaginary C Struct - word header
+#
+# struct Header
+# {
+#     char name[HEADER_NAME_BUF_SIZE];       // the name that is used to generate assembler labels
+#     char code[HEADER_CODE_BUF_SIZE];       // the code you write when you write forth 
+#     struct Header* next;
+#     struct Header* prev;
+#     Cell bImmediate;
+# }
 
-.equ WordNameBufLen, 32
-
-.macro WordHeader name, immediate
-\name :
-
-    1:
-    .ascii "\name"
-2:
-    .iflt WordNameBufLen - (2b - 1b)
-    .error "String too long"
-    .endif
-
-    .ifgt WordNameBufLen - (2b - 1b)
-    .zero WordNameBufLen - (2b - 1b)
-    .endif
-    #bIsImmediate: 
+.macro word_header_unitialised name, code, immediate
+\name:
+    padded_string \name, HEADER_NAME_BUF_SIZE
+    padded_string \code, HEADER_CODE_BUF_SIZE
+    .word 0
+    .word 0
     .word \immediate
-    #next:
-    .word 0
-    #prev:
-    .word 0
-
+\name\()_impl:
 .endm
 
-.equ SizeofWordHeader, (WordNameBufLen + (4*3))
+.macro word_header_last name, code, immediate, prev
+\name:
+    padded_string \name, HEADER_NAME_BUF_SIZE
+    padded_string \code, HEADER_CODE_BUF_SIZE
+    .word 0
+    .word \prev
+    .word \immediate
+\name\()_impl:
+.endm
 
-.equ Header_Name_Offset,        0
-.equ Header_IsImmediate_Offset, 32 
-.equ Header_Next_Offset,        32 
-.equ Header_Prev_Offset,        32 
 
-.section .data
+.macro word_header_first name, code, immediate, next
+\name:
+    padded_string \name, HEADER_NAME_BUF_SIZE
+    padded_string \code, HEADER_CODE_BUF_SIZE
+    .word \next
+    .word 0
+    .word \immediate
+\name\()_impl:
+.endm
 
-WordHeader "create", 0 
-Word_impl:
+.macro word_header name, code, immediate, next, prev
+\name:
+    padded_string \name, HEADER_NAME_BUF_SIZE
+    padded_string \code, HEADER_CODE_BUF_SIZE
+    .word \next
+    .word \prev
+    .word \immediate
+\name\()_impl:
+.endm
+
+.equ OFFSET_NAME, 32
+.equ OFFSET_CODE, 32
+.equ OFFSET_NEXT, 64
+.equ OFFSET_PREV, (64 + CELL_SIZE)
+.equ OFFSET_IMM, (64 + 2*CELL_SIZE)
+
+.macro word_next regPtrHeader, regPtrOut
+    addi \regPtrOut, \regPtrHeader, OFFSET_NEXT
+.endm
+
+.macro word_prev regPtrHeader, regPtrOut
+    addi \regPtrOut, \regPtrHeader, OFFSET_PREV
+.endm
+
+.macro word_imm regPtrHeader, regPtrOut
+    addi \regPtrOut, \regPtrHeader, OFFSET_IMM
+.endm
+
+.macro word_name regPtrHeader, regPtrOut
+    mv \regPtrOut, \regPtrHeader
+.endm
+
+.macro word_code regPtrHeader, regPtrOut
+    addi \regPtrOut, \regPtrHeader, OFFSET_CODE
+.endm
+
+.text
+
+vm_error_handler:
+    li a1, UART_BASE
+    la a0, vm_str_error_message
+    lw a0, 0(a0)
+    call puts
+1:
+    j 1b
+    ret
+
+find_dict_end:
+    # Args:
+    # a0 - ptr to first dict header
+    # Returns:
+    # a1 - ptr to last dict header
     SaveReturnAddress
-    la t0, dataStackPtr
-    mv t1, t0
+1:
+    word_next a0, t0
     lw t0, 0(t0)
-    addi t0, t0, SizeofWordHeader
-    sw t0, 0(t1)
+    mv a1, a0
+    mv a0, t0
+    bne t0, zero, 1b
     RestoreReturnAddress
     ret
+
+vm_init:
+    SaveReturnAddress
+    # init dictionary
+    la t0, vm_p_dictionary_start
+    la t1, emit
+    sw t1, 0(t0)
+
+    mv a0, t1
+    call find_dict_end
+
+    la t0, vm_p_dictionary_end
+    sw a1, 0(t0)
+
+    # init stacks
+    la t0, vm_p_data_stack_base
+    la t1, vm_data_stack
+    sw t1, 0(t0)
+
+    la t0, vm_p_return_stack_base
+    la t1, vm_return_stack
+    sw t1, 0(t0)
+    
+    RestoreReturnAddress
+    ret
+
+.macro end_word
+    addi s0, s0, 1
+    jal ra, vm_next
+.endm
+
+vm_next:
+    li t1, CELL_SIZE
+    mul t0, s0, t1
+    add t0, s1, t0
+    jalr ra, t0, 0
+    ret
+
+word_header_first emit,   emit,     0, key
+    la t0, vm_data_stack
+    la t1, vm_data_stack_size
+    lw t1, 0(t1)
+    PopStack t0, t1, a0
+    li a1, UART_BASE
+    call putc
+    end_word
+
+word_header       key,    key,      0, load, emit
+    li a0, UART_BASE
+    call getc_block
+    
+word_header_last  load,   "@",      0, key
