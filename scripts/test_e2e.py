985dc68c3dbc761c5149c9a44669b1a1d0bf9443
Jim Marshall <jimmarshall35@gmail.com>
2025-08-16 18:16:35 +0100

    CI test

---
+++ scripts/test_e2e.py
@@ -0,0 +1,23 @@
+import pexpect.popen_spawn
+
+
+def test_run():
+    assert True
+    proc = None
+    try:
+        proc = pexpect.popen_spawn.spawn("qemu-system-riscv32 -nographic -serial mon:stdio -machine virt -bios hello -qmp tcp:localhost:4444,server,wait=off", timeout=10)
+    except pexpect.ExceptionPexpect as e:
+        print(f"Error starting QEMU: {e}")
+        assert False, "QEMU failed to start"
+
+    
+    try:
+        proc.expect("Risc V Forth", timeout=10)
+    except pexpect.TIMEOUT:
+        print("Timeout waiting for 'Risc V Forth' prompt.")
+        assert False, "QEMU did not output expected prompt"
+    except pexpect.EOF:
+        print("pexpect.EOF")
+        assert False, "QEMU did not output expected prompt"
+
+    assert True
